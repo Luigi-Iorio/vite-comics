a730e5a3a8a6ed54d0dadd78092663d55c552d3e
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-07 15:44:40 +0100

    feat: header component

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,9 +1,51 @@
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      logo: "/img/dc-logo.png",
+      headerLinks: ["characters", "comics", "movies", "tv", "games", "collectibles", "videos", "fans", "news", "shop"]
+    }
+  }
+}
 </script>
 
 <template>
-  <h2>header</h2>
+  <header>
+    <nav>
+      <!-- logo -->
+      <div class="logo">
+        <img :src="logo" alt="logo dc">
+      </div>
+      <!-- /logo -->
+
+      <!-- header links -->
+      <ul>
+        <li v-for="link in headerLinks"><a href="#">{{ link }}</a></li>
+      </ul>
+      <!-- /header links -->
+    </nav>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  padding: 20px;
+  text-transform: uppercase;
+}
+
+nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1200px;
+  margin: 0 auto;
+
+}
+
+ul {
+  display: flex;
+  flex-direction: row;
+  gap: 20px
+}
+</style>
