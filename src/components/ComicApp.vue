8f6ef789a103be8c6ee3aac443537663518ac377
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-11 15:17:21 +0100

    feat: ComicApp componente per le card dei fumetti

---
+++ src/components/ComicApp.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    props: {
+        img: String,
+        title: String,
+    }
+}
+</script>
+<template>
+    <div>
+        <img :src="img" :alt="title">
+        <h3>{{ title }}</h3>
+    </div>
+</template>
+<style lang="scss" scoped></style>
